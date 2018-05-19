2cf31821ef0400b3ee8fb6054cd5f2d481fabab2
--global <amritendu.bhattacharjee@gmail.com>
2018-05-19 20:20:40 +0530

    new algo for calculations updated

--- app.js
+++ app.js
@@ -1,19 +1,44 @@
-var result = document.querySelector(".result");
-function getVal(val) {
-  if (val.value != "+" && val.value != "-" && val.value != "=") {
-    result.value += val.value;
-  }else{
-      var final = 0
-      var num = result.value
-      switch(val.value){
-          case '+':
-            var final = final + parseInt(num)
-            result.value = final
-          break;  
+var inputField = document.querySelector('.result')
+var finalOutput = 0;
+var operation;
 
+var moveDigits = function(tmp){
+
+}
+
+function inputValue(){
+  var inValue = inputField.value
+  return parseInt(inValue)
+}
+
+function operations(tmp){
+  var tmpNum = inputValue()
+  switch(tmp){
+    case '+':
+      operation = tmp
+      console.log(operation)
+      finalOutput = finalOutput + tmpNum
+      inputField.value = ' '
+      break
+    case '=':
+      if(operation == '+')
+      {
+       finalOutput = finalOutput + tmpNum
+       console.log(finalOutput)
+       display()
       }
   }
 }
 
+function display(){
+  inputField.value = finalOutput
+  finalOutput = 0
+}
 
-
+function getVal(el){
+  if(el.value==='+'||el.value==='-'||el.value==='*'||el.value==='/'||el.value==='='||el.value==='C'){
+    operations(el.value)
+  }else{
+    inputField.value += el.value
+  }
+}
