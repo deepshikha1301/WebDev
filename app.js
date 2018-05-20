9740979bec2be0de2d9ef50a0b12d3019b8673f9
deepshikha1301 <deepshikha2701@gmail.com>
2018-05-20 18:54:17 +0530

    functions updated

--- app.js
+++ app.js
@@ -1,44 +1,51 @@
 var inputField = document.querySelector('.result')
 var finalOutput = 0;
 var operation;
-
-var moveDigits = function(tmp){
-
-}
+var tmpNum1, tmpNum2;
 
 function inputValue(){
-  var inValue = inputField.value
-  return parseInt(inValue)
+  var inValue = inputField.value;
+  return parseInt(inValue);
 }
 
 function operations(tmp){
-  var tmpNum = inputValue()
   switch(tmp){
     case '+':
-      operation = tmp
-      console.log(operation)
-      finalOutput = finalOutput + tmpNum
-      inputField.value = ' '
-      break
-    case '=':
-      if(operation == '+')
-      {
-       finalOutput = finalOutput + tmpNum
-       console.log(finalOutput)
-       display()
-      }
+      finalOutput = tmpNum1 + tmpNum2;
+      display();
+      break;
+    case '-':
+      finalOutput = tmpNum1 - tmpNum2;
+      display();
+      break;
+    case 'x':
+      finalOutput = tmpNum1 * tmpNum2;
+      display();
+      break;
+    case '/':
+      finalOutput = tmpNum1 / tmpNum2;
+      display();
+      break;   
   }
 }
 
 function display(){
-  inputField.value = finalOutput
-  finalOutput = 0
+  inputField.value = finalOutput;
+  finalOutput = 0;
 }
 
 function getVal(el){
-  if(el.value==='+'||el.value==='-'||el.value==='*'||el.value==='/'||el.value==='='||el.value==='C'){
-    operations(el.value)
-  }else{
-    inputField.value += el.value
+  if(el.value==='+'||el.value==='-'||el.value==='x'||el.value==='/'){
+    tmpNum1 = inputValue();
+    operation = el.value;
+    inputField.value = ' ';
+  }
+  else if(el.value==='=')
+  {
+    tmpNum2 = inputValue();
+    operations(operation);
+  }
+  else{
+    inputField.value += el.value;
   }
 }
